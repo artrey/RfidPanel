f0920f854d2fb5f417d2a6ab9ff29b58b2e31ce2
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2019-05-22 12:36:20 +0300

    Started GUI

--- RfidPanel/MainWindow.xaml.cs
+++ RfidPanel/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace RfidPanel
 {
@@ -17,19 +18,36 @@ namespace RfidPanel
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            _device.UidReceived += UidReceived;
-            _device.Open(new Configuration { PortName = "COM16", BaudRate = 115200 });
+            //_device.UidReceived += UidReceived;
+            //_device.Open(new Configuration { PortName = "COM16", BaudRate = 115200 });
+            UidReceived(null, "0D 12 43 F1");
+            SetErrorMessage("sdgsdfgsdfg");
+            UID.Text = "gdfgsg";
+        }
+
+        private void SetErrorMessage(string mes)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                Error.Text = mes;
+            });
+        }
+
+        private void HideErrorMessage()
+        {
+            SetErrorMessage(string.Empty);
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            _device.UidReceived -= UidReceived;
-            _device.Close();
+            //_device.UidReceived -= UidReceived;
+            //_device.Close();
+            HideErrorMessage();
         }
 
         private void UidReceived(object sender, string e)
         {
-            Dispatcher.Invoke(() => ListBox.Items.Add(e));            
+            //Dispatcher.Invoke(() => ListBox.Items.Add(e));            
         }
     }
 }
